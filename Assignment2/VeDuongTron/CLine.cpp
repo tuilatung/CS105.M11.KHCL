e52ed50531b45c6dea1255da9dfc84e5c066aef0
Tung Huynh <19522492@gm.uit.edu.vn>
2021-09-23 16:41:17 +0700

    nothing

--- Assignment2/VeDuongTron/CLine.cpp
+++ Assignment2/VeDuongTron/CLine.cpp
@@ -13,27 +13,27 @@ void CLine::_MidPoint(CDC* pDC, int x_center, int y_center, float r, COLORREF co
     int x = 0, y = r;
     // ve 4 diem ban dau la 4 goc phan tu 
     pDC->SetPixel(x + x_center, y + y_center, color);
-	pDC->SetPixel(x + x_center, -y + y_center, color);
-	pDC->SetPixel(y + x_center, x + y_center, color);
-	pDC->SetPixel(-y + x_center, x + y_center, color);
+    pDC->SetPixel(x + x_center, -y + y_center, color);
+    pDC->SetPixel(y + x_center, x + y_center, color);
+    pDC->SetPixel(-y + x_center, x + y_center, color);
     int f = 1 - r;
     while (x <= (r / sqrt(2)))
     {
         x++;
-		if (f < 0)
-			f = f + (x << 1) + 1;
+        if (f < 0)
+            f = f + (x << 1) + 1;
         else
         {
             y--;
-			f = f + (x << 1) - (y << 1) + 5;
+		    f = f + (x << 1) - (y << 1) + 5;
         }
         pDC->SetPixel(x + x_center, y + y_center, color);
         pDC->SetPixel(-x + x_center, y + y_center, color);
         pDC->SetPixel(x + x_center, -y + y_center, color);
         pDC->SetPixel(-x + x_center, -y + y_center, color);
-		pDC->SetPixel(y + x_center, x + y_center, color);
-		pDC->SetPixel(-y + x_center, x + y_center, color);
-		pDC->SetPixel(y + x_center, -x + y_center, color);
-		pDC->SetPixel(-y + x_center, -x + y_center, color);
+	    pDC->SetPixel(y + x_center, x + y_center, color);
+	    pDC->SetPixel(-y + x_center, x + y_center, color);
+	    pDC->SetPixel(y + x_center, -x + y_center, color);
+	    pDC->SetPixel(-y + x_center, -x + y_center, color);
     }
 }
